da97bb11043722ec8cc46bbcd7f1c6978a74ee5c
krithi-sa31 <krithisa79@gmail.com>
2025-02-18 15:36:23 +0530

    c prog

---
+++ reverse_sol2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int findlength(char *s)
+{
+    int length=0; 
+    for(char *p_ch=s;(*p_ch)!='\0';p_ch++)
+    {
+        length++;
+    }
+    return length;
+}
+
+void reversestring(char *s)
+{
+    int len=findlength(s);
+    for(int left=0,right=len-1;left<right;left++,right--)
+    {
+        char temp=*(s+left);
+        *(s+left)=*(s+right);
+        *(s+right)=temp;
+    }
+}
+
+int main()
+{
+    char s[255];
+    printf("enter the string:");
+    scanf("%s",s);
+    printf("before reversing:%s\n",s);
+    reversestring(s);
+    printf("reversed string:%s\n",s);
+    return 0;
+
+}
